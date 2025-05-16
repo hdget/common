ef3d2c1d397a751106bb7091016d215dfb712068
rfancn <reg_info@qq.com>
2025-05-16 14:14:58 +0800

    add constant

---
+++ constant/meta.go
@@ -0,0 +1,10 @@
+package constant
+
+const (
+	MetaKeyClient  = "hd-client"
+	MetaKeyRelease = "hd-release"
+	MetaKeyTsn     = "hd-tsn" // tenant sn
+	MetaKeyUsn     = "hd-usn" // user sn
+	MetaKeyRsn     = "hd-rsn" // encoded role ids
+	MetaKeyCaller  = "dapr-caller-app-id"
+)
