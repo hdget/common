39f72ed221b0002e788d1279dea3a6cc2a2cb6a8
Ryan Fan <reg_info@qq.com>
2025-06-09 13:17:40 +0800

    Feat/global (#13)
    * add meta app id
    * add config key

--- intf/provider_config.go
+++ intf/provider_config.go
@@ -2,5 +2,6 @@ package intf
 
 type ConfigProvider interface {
 	Provider
-	Unmarshal(configVar any, key ...string) error
+	Unmarshal(configVar any, key ...string) error // 读取配置到变量configVar
+	Get(key string) any                           // 获取配置项的值
 }
