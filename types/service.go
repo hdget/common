a57b11e17e18e257e9515271fb0b1ad241ac8b45
rfancn <reg_info@qq.com>
2025-09-06 19:03:04 +0800

    debug

--- types/service.go
+++ types/service.go
@@ -5,8 +5,8 @@ import (
 )
 
 type BizSvcQueryResponse[BizObject any] struct {
-	Total int64
-	Items []BizObject
+	Total int64       `json:"total"`
+	Items []BizObject `json:"items"`
 }
 
 type BizSvcOperation[BizObject any] interface {
