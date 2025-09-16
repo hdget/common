1c88fe3c2a76c4dcc8dc4a55a1e47cd4fceb2e07
Ryan Fan <reg_info@qq.com>
2025-09-16 13:09:44 +0800

    add getclient (#31)
    Co-authored-by: hdget <hdget@hdget.com>

--- meta/meta_reader_grpc.go
+++ meta/meta_reader_grpc.go
@@ -12,23 +12,19 @@ type grpcCtxReaderImpl struct {
 
 func FromGrpcContext(ctx context.Context) MetaReader {
 	// 尝试从grpc context中获取meta信息
+	var metaMap map[string]string
 	if md, exists := metadata.FromIncomingContext(ctx); exists {
-		metas := make(map[string]string)
+		metaMap = make(map[string]string)
 		for key, values := range md {
 			if len(values) > 0 {
-				metas[key] = values[0]
+				metaMap[key] = values[0]
 			}
-		}
-		return &grpcCtxReaderImpl{
-			metaCtxReaderImpl: &metaCtxReaderImpl{
-				metas: metas,
-			},
 		}
 	}
 
 	return &grpcCtxReaderImpl{
 		metaCtxReaderImpl: &metaCtxReaderImpl{
-			metas: make(map[string]string),
+			metaMap: metaMap,
 		},
 	}
 }
