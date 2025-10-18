19c98bc36da30af68a851778b1febdd9bf3c4f7b
Ryan Fan <reg_info@qq.com>
2025-10-18 22:00:29 +0800

    reconstruct transactor (#35)
    Co-authored-by: hdget <hdget@hdget.com>

--- biz/transactor.go
+++ biz/transactor.go
@@ -3,10 +3,10 @@ package biz
 import "sync"
 
 type transactor interface {
-	Ref(tx any)  // 引用, 因为可能嵌套NewTransactor, 这里加入计数
-	Unref() bool // 解引用, 如果计数为0，表示需要Finalize，并Destroy tx
-	Get() any    // 获取Tx
-	Destroy()    // 销毁tx
+	Ref(tx any)      // 引用, 因为可能嵌套NewTransactor, 这里加入计数
+	Unref()          // 解引用, 如果计数为0，表示需要Finalize，并Destroy tx
+	GetTx() any      // 获取Tx
+	ReachRoot() bool // 是否到达最外层
 }
 
 type safeTxImpl struct {
@@ -30,7 +30,7 @@ func (t *safeTxImpl) Ref(tx any) {
 	t.count++
 }
 
-func (t *safeTxImpl) Unref() bool {
+func (t *safeTxImpl) Unref() {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 
@@ -38,20 +38,22 @@ func (t *safeTxImpl) Unref() bool {
 		t.count--
 	}
 
+	// 如果计数为0后销毁tx
+	if t.count == 0 {
+		t.tx = nil
+	}
+}
+
+func (t *safeTxImpl) ReachRoot() bool {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
 	// 为0表示需要Commit Or Rollback
 	return t.count <= 0
 }
 
-func (t *safeTxImpl) Get() any {
+func (t *safeTxImpl) GetTx() any {
 	t.mu.RLock() // 使用读锁，允许多个 Get 调用并发执行
 	defer t.mu.RUnlock()
 	return t.tx
-}
-
-func (t *safeTxImpl) Destroy() {
-	t.mu.Lock()
-	defer t.mu.Unlock()
-	if t.count == 0 { // 检查计数是否为 0
-		t.tx = nil
-	}
 }
