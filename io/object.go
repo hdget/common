9d50fd16f3bf891b83f780690f09df934cb1b3be
rfancn <reg_info@qq.com>
2025-12-20 10:23:49 +0800

    add object and object_ref

---
+++ io/object.go
@@ -0,0 +1,37 @@
+package io
+
+import "github.com/hdget/common/protobuf"
+
+/* request */
+
+type GetObjectRequest struct {
+	Id int64 `json:"id"`
+}
+
+type BulkGetObjectRequest struct {
+	Ids []int64 `json:"ids"`
+}
+
+type ApproveObjectRequest struct {
+	Id int64 `json:"id"`
+}
+
+type DeleteObjectRequest struct {
+	Id int64 `json:"id"`
+}
+
+type QueryObjectRequest struct {
+	Filters map[string]string   `json:"filters"`
+	List    *protobuf.ListParam `json:"list"`
+}
+
+/* response */
+
+type CreateObjectResponse struct {
+	Id int64 `json:"id"`
+}
+
+type QueryObjectResponse[BizObject any] struct {
+	Total int64       `json:"total"`
+	Items []BizObject `json:"items"`
+}
---
+++ io/object_ref.go
@@ -0,0 +1,38 @@
+package io
+
+import "github.com/hdget/common/protobuf"
+
+/* request */
+
+type CreateRefObjectRequest[BizObject any] struct {
+	Id   int64     `json:"id"`
+	Item BizObject `json:"item"`
+}
+
+type EditRefObjectRequest[BizObject any] struct {
+	Id   int64     `json:"id"`
+	Item BizObject `json:"item"`
+}
+
+type DeleteRefObjectRequest struct {
+	Id     int64 `json:"id"`
+	ItemId int64 `json:"itemId"`
+}
+
+type GetRefObjectRequest struct {
+	Id     int64 `json:"id"`
+	ItemId int64 `json:"itemId"`
+}
+
+type QueryRefObjectRequest struct {
+	Id      int64               `json:"id"`
+	Filters map[string]any      `json:"filters"`
+	List    *protobuf.ListParam `json:"list"`
+}
+
+/* response */
+
+type QueryRefObjectResponse[BizObject any] struct {
+	Total int64       `json:"total"`
+	Items []BizObject `json:"items"`
+}
