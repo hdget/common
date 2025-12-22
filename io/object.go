f857f9b3b2e685abd393753ed9de7f03a037f55e
Ryan Fan <reg_info@qq.com>
2025-12-22 08:37:43 +0800

    add multiple unique key (#47)
    * add multiple unique key

--- io/object.go
+++ io/object.go
@@ -5,24 +5,32 @@ import "github.com/hdget/common/protobuf"
 /* request */
 
 type GetObjectRequest struct {
-	Id int64 `json:"id"`
+	Id   int64  `json:"id,omitempty"`
+	Sn   string `json:"sns,omitempty"`
+	Name string `json:"name,omitempty"`
 }
 
 type BulkGetObjectRequest struct {
-	Ids []int64 `json:"ids"`
+	Ids   []int64  `json:"ids,omitempty"`
+	Sns   []string `json:"sns,omitempty"`
+	Names []string `json:"names,omitempty"`
 }
 
 type ApproveObjectRequest struct {
-	Id int64 `json:"id"`
+	Id   int64  `json:"id,omitempty"`
+	Sn   string `json:"sns,omitempty"`
+	Name string `json:"name,omitempty"`
 }
 
 type DeleteObjectRequest struct {
-	Id int64 `json:"id"`
+	Id   int64  `json:"id,omitempty"`
+	Sn   string `json:"sns,omitempty"`
+	Name string `json:"name,omitempty"`
 }
 
 type QueryObjectRequest struct {
-	Filters map[string]any      `json:"filters"`
-	List    *protobuf.ListParam `json:"list"`
+	Filters map[string]any      `json:"filters,omitempty"`
+	List    *protobuf.ListParam `json:"list,omitempty"`
 }
 
 /* response */
